7202f8f607d159792b141538dc85254492b9ce82
JIMUIA <juanignacio.melendez9@gmail.com>
2022-11-22 19:29:05 -0600

    editado pruebas unitarias

---
+++ peliculas-react/src/busqueda.test.js
@@ -0,0 +1,11 @@
+import { MovieCard } from "./MovieCard";
+import { MovieDetails } from "./MovieDetails";
+
+describe('funcion de busqueda', ()  => {
+    it('busqueda correcta',() => {
+        const search = 
+        MovieCard(MovieDetails)
+        expect(search).imageUrl(MovieDetails)
+
+    })
+});
